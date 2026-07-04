1f6718a7894ef00103a5feb61c613aac35b59238
iki1uc <entertrainer@web.de>
2026-07-04 20:23:22 +0200

    Update zum.js

--- zum.js
+++ zum.js
@@ -1,1 +1,44 @@
+// SCAN – Funktionsmodul unter modi + nameLage
+
+import { AX_forward } from "./AX/forward.js";
+import { IS_state } from "./IS/state.js";
+import { XA_return } from "./XA/return.js";
+import { SYT_core } from "./SYT/core.js";
+
+export const SCAN = {
+    // NAME + LAGE für SCAN
+    nameLage: {
+        name: "SCAN",
+        lage: {
+            type: "Funktionsmodul",
+            axis: "modi",
+            moves: ["AX", "IS", "XA", "SYT"]
+        },
+        status: "Funktionsbereit",
+        stabilitaet: 0.97
+    },
+
+    AX() {
+        return AX_forward();
+    },
+
+    IS() {
+        return IS_state();
+    },
+
+    XA() {
+        return XA_return();
+    },
+
+    SYT() {
+        return SYT_core();
+    },
+
+    status() {
+        return {
+            functions: ["AX", "IS", "XA", "SYT"],
+            nameLage: this.nameLage
+        };
+    }
+};
 
